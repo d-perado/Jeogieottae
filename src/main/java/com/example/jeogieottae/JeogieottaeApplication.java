fe01b9df7be24b1331ea8770a79efa3b03e5b438
d-perado <sjm01065@naver.com>
2026-01-03 20:34:47 +0900

    ㄹ

--- src/main/java/com/example/jeogieottae/JeogieottaeApplication.java
+++ src/main/java/com/example/jeogieottae/JeogieottaeApplication.java
@@ -10,7 +10,7 @@ public class JeogieottaeApplication {
 
     public static void main(String[] args) {
 
-        System.out.println("성공하면말해다오1221");
+        System.out.println("성공하면말해다오12231");
         SpringApplication.run(JeogieottaeApplication.class, args);
     }
 
