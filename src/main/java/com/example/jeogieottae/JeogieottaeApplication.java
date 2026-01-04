b807ba2a481d81186429ecfc1653e1baeb994343
d-perado <sjm01065@naver.com>
2026-01-05 03:30:25 +0900

    버퍼용 이미지 제외 삭제

--- src/main/java/com/example/jeogieottae/JeogieottaeApplication.java
+++ src/main/java/com/example/jeogieottae/JeogieottaeApplication.java
@@ -10,6 +10,7 @@ public class JeogieottaeApplication {
 
     public static void main(String[] args) {
 
+        System.out.println("성ㄹㄹff");
         SpringApplication.run(JeogieottaeApplication.class, args);
     }
 
